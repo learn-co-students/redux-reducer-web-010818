db2d123c386011d49b2119284e4f1df8e972e39d
TorreJohnson <torre.johnson@gmail.com>
2018-03-29 09:41:02 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      const filteredFriends = state.friends.filter(
+        friend => friend.id !== action.id
+      );
+      return { friends: [...filteredFriends] };
+    default:
+      return state;
+  }
 }
